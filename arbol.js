d616e11a606cf4f5fb231e60c53e9a72f65eeb8b
Castillo <dcastillo10@ucol.mx>
2022-11-27 17:37:29 -0600

    Add clases NODO y ARBOL

--- arbol.js
+++ arbol.js
@@ -1,1 +1,26 @@
+class Nodo{
+    constructor(numero){
+        this.numero=numero;
+        this.hijoIzquierda=null;
+        this.hijoDerecha=null;
+        this.izquierda=null;
+        this.derecha=null;
+    }
+}
+
+
+class Arbol{
+    constructor(){
+        this.raiz=null;
+    }
+
+    //1.- Tomar la expresión y generar el árbol binario, imprimiendo la notación preorder y postorder
+    preOrder(){ //RID
+
+    }
+
+    postOrder(){
+        
+    }
+}
 
