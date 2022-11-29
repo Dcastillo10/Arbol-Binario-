39ecda2d579b8a400a1c5c6a82d0e930e6acd837
Castillo <dcastillo10@ucol.mx>
2022-11-28 22:29:29 -0600

    Add createArbol Method and printing expression

--- arbol.js
+++ arbol.js
@@ -1,6 +1,6 @@
 class Nodo{
     constructor(num){
-        this.num=num;
+        this.num=num;//PUEDE TRATARSE DE UN NUMERO O DE UN OPERADOR
         this.hijoIzquierda=null;
         this.hijoDerecha=null;
         this.izquierda=null;
@@ -19,9 +19,8 @@ class Arbol{
     //1.- Tomar la expresión y generar el árbol binario, imprimiendo la notación preorder y postorder
     preOrder(){ //RID
 
-
     }
-
+    //1.- Tomar la expresión y generar el árbol binario, imprimiendo la notación preorder y postorder
     postOrder(){
 
     }
@@ -44,7 +43,7 @@ class Arbol{
             this.ultimo=num;
         }
         else { 
-            this.ultimo.derecha=num; //El siguiente del que era el ultimo será el nuevo nodo
+            this.ultimo.derecha=num; //El derechauiente del que era el ultimo será el nuevo nodo
             num.izquierda=this.ultimo;
             this.ultimo=num;
         }
@@ -52,16 +51,85 @@ class Arbol{
 
     print(){
         let list="";
+        //Creamos la función print  
         let temp=this.primero;
             while (temp) {
-                list += temp.num + "  ";
-                temp = temp.derecha;
+                list+=`${temp.num} `;
+                temp=temp.derecha;
             }
         return list;
     }
+
+    createArbol(){
+        let temp=this.primero;
+
+        while (temp) {
+            if (temp.num=="*" || temp.num== "/") { //POR JERARQUÍA DE OPERACIONES, LOS PRIMEROS OPERADORES QUE DEBEMOS TOMAS SON * Y /
+                temp.hijoIzquierda = temp.izquierda;//CREAMOS EL ARBOL CON AYUDA DE LAS LISTAS DOBLES
+                temp.hijoDerecha = temp.derecha;
+
+                if (temp.hijoIzquierda.num === this.primero.num || temp.hijoDerecha.num === this.primero.num) {
+                    this.primero = temp;
+                }
+
+                if (temp.derecha.derecha !== null) {
+                    temp.derecha.derecha.izquierda = temp;
+                    temp.derecha = temp.derecha.derecha;
+                }
+                else {
+                    temp.derecha = null;
+                }
+
+                if (temp.izquierda.izquierda !== null) {
+                    temp.izquierda.izquierda.derecha = temp;
+                    temp.izquierda = temp.izquierda.izquierda;
+                }
+                else {
+                    temp.izquierda = null;
+                }
+                //console.log(`Expresión pendiente:  ${arbolito1.print()}`);
+            }
+            temp = temp.derecha;
+        }
+        temp=this.primero;
+
+        while (temp) {
+            if (temp.num === "+" || temp.num === "-") {
+                temp.hijoIzquierda = temp.izquierda;
+                temp.hijoDerecha = temp.derecha;
+                if (temp.hijoIzquierda.num === this.primero.num || temp.hijoDerecha.num === this.primero.num) {
+                    this.primero = temp;
+                }
+
+                if (temp.derecha.derecha != null) {
+                    temp.derecha.derecha.izquierda = temp;
+                    temp.derecha = temp.derecha.derecha;
+                }
+                else {
+                    temp.derecha = null;
+                }
+
+                if (temp.izquierda.izquierda != null) {
+                    temp.izquierda.izquierda.derecha = temp;
+                    temp.izquierda = temp.izquierda.izquierda;
+                }
+                else {
+                    temp.izquierda = null;
+                }
+                //console.log(`Expresión pendiente:  ${arbolito1.print()}`);
+            }
+            temp = temp.derecha;
+        }
+        this.raiz = this.primero;
+    }
+    
 }
 
 
 let arbolito1=new Arbol();
 arbolito1.splitExpression("2*3+4-5+6");
 console.log(arbolito1.print());
+//console.log(arbolito1.createArbol());
+
+
+
